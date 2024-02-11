50ef0c17640f33457b730c9f3bb96bb006742faa
fcanlnony <1923170329@qq.com>
2024-02-11 18:14:18 +0800

    default theme: kaolin-theme

--- init.el
+++ init.el
@@ -1,10 +1,8 @@
 (load-file "~/.emacs.d/configuration/config/emacs.base-config.el")
 (add-to-list 'default-frame-alist '(font . "Noto Sans Mono-12.9"))
 (setq package-selected-packages
-       '(all-the-icons dashboard highlight-indent-guides treemacs projectile rainbow-delimiters smex use-package markdown-mode beacon yasnippet corfu corfu-terminal imenu-list))
-(add-to-list 'load-path "~/.emacs.d/plugins/simple-theme")
-(require 'simple-theme-theme)
-(load-theme 'simple-theme t)
+       '(all-the-icons dashboard highlight-indent-guides treemacs projectile rainbow-delimiters smex use-package markdown-mode beacon yasnippet corfu corfu-terminal imenu-list kaolin-theme))
+(load-theme 'kaolin-light t)
 (load-file "~/.emacs.d/configuration/config/emacs.extra-config.el")
 (load-file "~/.emacs.d/configuration/config/emacs.dashboard-config.el")
 (load-file "~/.emacs.d/configuration/config/emacs.indent-guide-config.el")

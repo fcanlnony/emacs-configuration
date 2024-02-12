43351bea3e7e25a407f6a038f9f4d379276a3e45
fcanlnony <1923170329@qq.com>
2024-02-12 11:29:53 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-cp -r config ~/.emacs.d/configuration/
-cp -r elisp ~/.emacs.d/configuration/
+cp -r config/ ~/.emacs.d/configuration/
+cp -r elisp/ ~/.emacs.d/configuration/
 cp init.el ~/.emacs.d/configuration/
 git clone --depth=1 https://github.com/manateelazycat/awesome-tab.git ~/.emacs.d/plugins/awesome-tab
